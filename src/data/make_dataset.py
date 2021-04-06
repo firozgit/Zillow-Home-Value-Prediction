1f4ebd45a962db510b107485a0c44d76d5dde7a9
Firoz Shaik <firoz.shaik026@gmail.com>
2021-04-06 19:35:56 -0400

    formatting changes

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 
-"""get_data definition to do the following
+"""get_data definition does the following
 
 1. Read the train/properties data
 2. Merge the datasets
@@ -10,7 +10,8 @@ import pandas as pd
 4. Convert the categorical features to "object" data type.
 
 
- """
+"""
+
 
 def read_data(data_string):
     """
@@ -26,12 +27,11 @@ def read_data(data_string):
     
     """
     df = pd.read_csv("../data/raw/{0}.csv".format(data_string))
-    
+
     return df
 
 
-
-def remove_duplicate_parcels(df):    
+def remove_duplicate_parcels(df):
     """
     Remove the records with duplicate parcelid in the merged train data set.
     
@@ -41,19 +41,22 @@ def remove_duplicate_parcels(df):
     Returns:    
     unique_df -- a dataframe with unique parcelid's
     
-    """    
+    """
     parcel_count = df.groupby(["parcelid"]).size()
     unique_parcel = df[df["parcelid"].isin(parcel_count[parcel_count == 1].index)]
     duplicated_parcel = df[df["parcelid"].isin(parcel_count[parcel_count > 1].index)]
-    duplicated_parcel_made_unique = duplicated_parcel.sample(frac=1, random_state=42).groupby(["parcelid"]).head(1)
+    duplicated_parcel_made_unique = (
+        duplicated_parcel.sample(frac=1, random_state=42).groupby(["parcelid"]).head(1)
+    )
     unique_df = pd.concat([unique_parcel, duplicated_parcel_made_unique], axis=0)
-    
+
     return unique_df
-    
+
 
 def get_data(data_string):
     """
-    Read the train/test dataset and merge with properties data set and remove duplicate parcelid's in train
+    Read the train/test dataset and merge with properties data set 
+    and remove duplicate parcelid's in train
     
     Parameters:
     data_string -- "train" or "test" 
@@ -62,34 +65,55 @@ def get_data(data_string):
     X, y -- a tuple of dataframe X and Series y
     
     
-    """         
+    """
     year = 2016 if data_string == "train" else 2017
-        
+
     train = read_data("train_{0}".format(year))
     properties = read_data("properties_{0}".format(year))
     merged = pd.merge(train, properties, on="parcelid", how="left")
-                      
+
     if data_string == "train":
         merged = remove_duplicate_parcels(merged)
-                          
-    y = merged["logerror"]                          
-    merged = merged.drop(columns=["logerror"], axis=1) 
-    
+
+    y = merged["logerror"]
+    merged = merged.drop(columns=["logerror"], axis=1)
+
     id_col = ["parcelid"]
-    cat_cols = ["airconditioningtypeid", "architecturalstyletypeid", "buildingclasstypeid", 
-                "buildingqualitytypeid", "decktypeid", "fips", "fireplaceflag", 
-                "hashottuborspa", "heatingorsystemtypeid", "pooltypeid10", "pooltypeid2",
-                "pooltypeid7", "propertycountylandusecode", "propertylandusetypeid", 
-                "propertyzoningdesc", "rawcensustractandblock", "censustractandblock", 
-                "regionidcounty", "regionidcity", "regionidzip", "regionidneighborhood", 
-                "storytypeid", "typeconstructiontypeid", "yearbuilt", "assessmentyear", 
-                "taxdelinquencyflag", "taxdelinquencyyear"]
+    cat_cols = [
+        "airconditioningtypeid",
+        "architecturalstyletypeid",
+        "buildingclasstypeid",
+        "buildingqualitytypeid",
+        "decktypeid",
+        "fips",
+        "fireplaceflag",
+        "hashottuborspa",
+        "heatingorsystemtypeid",
+        "pooltypeid10",
+        "pooltypeid2",
+        "pooltypeid7",
+        "propertycountylandusecode",
+        "propertylandusetypeid",
+        "propertyzoningdesc",
+        "rawcensustractandblock",
+        "censustractandblock",
+        "regionidcounty",
+        "regionidcity",
+        "regionidzip",
+        "regionidneighborhood",
+        "storytypeid",
+        "typeconstructiontypeid",
+        "yearbuilt",
+        "assessmentyear",
+        "taxdelinquencyflag",
+        "taxdelinquencyyear",
+    ]
 
     # convert all categorical variables to categorical type
-    for col in (id_col + cat_cols):
+    for col in id_col + cat_cols:
         merged[col] = merged[col].astype("object")
 
-    # convert transactiondate column to datetime type    
-    merged["transactiondate"] = pd.to_datetime(merged["transactiondate"])    
+    # convert transactiondate column to datetime type
+    merged["transactiondate"] = pd.to_datetime(merged["transactiondate"])
 
     return merged, y
--- src/models/tune_model.py
+++ src/models/tune_model.py
@@ -1,7 +1,7 @@
 import sys
 import inspect
 
-#Add the scripts directory to the sys path
+# Add the scripts directory to the sys path
 sys.path.append("../features")
 from data_processor import DataProcessor
 
@@ -13,18 +13,22 @@ import xgboost as xgb
 from sklearn.metrics import mean_absolute_error
 
 """
-modelfit function - Gets the number of boosting rounds using XGBoost Cross Validation Method
-xgb_gridsearch function - Get the best parameters of the XGBoost model using Pipeline, GridSearchCV and DataProcessor class
+modelfit function - Gets the number of boosting rounds 
+using XGBoost Cross Validation Method
+
+xgb_gridsearch function - Get the best parameters of the XGBoost model 
+using Pipeline, GridSearchCV and DataProcessor class
 
 """
 
 
-
 def modelfit(xgb_reg, X_train, y_train, X_test, y_test):
     """
-    Get the number of boosting rounds of the model using XGBoost Cross Validation Method
+    Get the number of boosting rounds of the model 
+    using XGBoost Cross Validation Method
     
-    Use the above parameters to Fit the XGBoost model on the train and get feature importance and predictons
+    Use the above parameters to Fit the XGBoost model on the train 
+    and get feature importance and predictons
        
     Parameters:
     xgb_reg model, X_train, y_train, X_test, y_test datasets
@@ -34,41 +38,51 @@ def modelfit(xgb_reg, X_train, y_train, X_test, y_test):
     None
         
     """
-    
+
     xgb_param = xgb_reg.get_xgb_params()
-    
-    num_boost_round = xgb_reg.get_params()['n_estimators']
+
+    num_boost_round = xgb_reg.get_params()["n_estimators"]
     num_boost_round
 
-    xgb_train = xgb.DMatrix(X_train, y_train) 
+    xgb_train = xgb.DMatrix(X_train, y_train)
 
-    cv_results = xgb.cv(xgb_param, xgb_train, num_boost_round=num_boost_round, nfold=3, metrics='mae', early_stopping_rounds=50)
+    cv_results = xgb.cv(
+        xgb_param,
+        xgb_train,
+        num_boost_round=num_boost_round,
+        nfold=3,
+        metrics="mae",
+        early_stopping_rounds=50,
+    )
 
     # print("\n---- cv results ----\n\n", cv_results)
-    # print("\nCV test results best - MAE: {}, ".format(cv_results['test-mae-mean'].min()))
+    # print(
+    #     "\nCV test results best - MAE: {}, ".format(cv_results["test-mae-mean"].min())
+    # )
     print("\nBest estimators :", cv_results.shape[0])
-    
+
     xgb_reg.set_params(n_estimators=cv_results.shape[0])
 
-    xgb_reg.fit(X_train, y_train, eval_metric='mae')
+    xgb_reg.fit(X_train, y_train, eval_metric="mae")
 
     dtrain_pred = xgb_reg.predict(X_train)
 
     print("\nTrain MAE: {}".format(mean_absolute_error(y_train, dtrain_pred)))
-    
+
     dtest_pred = xgb_reg.predict(X_test)
-    
+
     print("\nTest MAE: {}".format(mean_absolute_error(y_test, dtest_pred)))
 
-    plt.figure(figsize=(20,15))
+    plt.figure(figsize=(20, 15))
     xgb.plot_importance(xgb_reg, ax=plt.gca())
-    
+
     return None
-    
+
 
 def xgb_gridsearch(param_test, xgb_reg):
     """
-    Get the best parameters of the XGBoost model using Pipeline, GridSearchCV and DataProcessor class
+    Get the best parameters of the XGBoost model using Pipeline, GridSearchCV 
+    and DataProcessor class
     
     Parameters:
     param_test, xgb_reg model
@@ -78,26 +92,40 @@ def xgb_gridsearch(param_test, xgb_reg):
     None
         
     """
-    
-    dp = DataProcessor(cols_to_remove=["parcelid", "propertyzoningdesc", "rawcensustractandblock", "regionidneighborhood", "regionidzip", "censustractandblock"], 
-                      datecol="transactiondate")
 
-    pipeline = Pipeline([
-        ("dataprocessor", dp),
-        ("xgb_reg", xgb_reg)
-    ])
+    dp = DataProcessor(
+        cols_to_remove=[
+            "parcelid",
+            "propertyzoningdesc",
+            "rawcensustractandblock",
+            "regionidneighborhood",
+            "regionidzip",
+            "censustractandblock",
+        ],
+        datecol="transactiondate",
+    )
 
-    grid_search = GridSearchCV(estimator=pipeline, param_grid=param_test, scoring="neg_mean_absolute_error", 
-                               n_jobs=-1, cv=3, verbose=1)
+    pipeline = Pipeline([("dataprocessor", dp), ("xgb_reg", xgb_reg)])
+
+    grid_search = GridSearchCV(
+        estimator=pipeline,
+        param_grid=param_test,
+        scoring="neg_mean_absolute_error",
+        n_jobs=-1,
+        cv=3,
+        verbose=1,
+    )
 
     grid_search.fit(X_train, y_train)
 
     print("----- Grid Search cv results ----- \n")
-    for mean_score, params in zip(grid_search.cv_results_["mean_test_score"], grid_search.cv_results_["params"]):
+    for mean_score, params in zip(
+        grid_search.cv_results_["mean_test_score"], grid_search.cv_results_["params"]
+    ):
         print(-(mean_score), params)
 
     print("\n----- Grid Search best parameters ------ \n", grid_search.best_params_)
     print("\n")
     print("----- Grid Search best score ------ \n", -(grid_search.best_score_))
-    
+
     return None
